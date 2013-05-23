5e9f932f1e889cb7dc2a15d4dc345976de1fae1f
amitmurthy <amit.murthy@gmail.com>
2013-05-23 12:59:10 +0530

    S3 work

--- src/S3.jl
+++ src/S3.jl
@@ -1,34 +1,44 @@
-type S3_Response_State
-    Content-Length::Int  
+# Etag should always be quoted - in XML as well as headers...
+# handle Content-MD5 for IO stream objects too...
+
+type S3Error
+    code::String
+    message::String
+    resource::String
+    request_id::String
+end
+
+type S3Response
+    content_length::Int  
     #Connection (open or closed) - we will not use this  
-    Date::String    #The date and time Amazon S3 responded, for example, Wed, 01 Mar 2009 12:00:00 GMT.
-    Server::String  #The name of the server that created the response.
-    ETag::String
+    date::String    #The date and time Amazon S3 responded, for example, Wed, 01 Mar 2009 12:00:00 GMT.
+    server::String  #The name of the server that created the response.
+    etag::String
+    http_code::Int
+# Define common amz fields here
+
+# all header fields
+    headers::Dict
     
-# x-amz- fields     
-    delete_marker::Bool         # Specifies whether the object returned was (true) or was not (false) a Delete Marker.
-    id_2::String                # A special token that helps AWS troubleshoot problems.
-    request_id::String          # A value created by Amazon S3 that uniquely identifies the request. 
-    version_id::String          # The version of the object.
-
+# All header fields    
+    obj::Any
+    error::Union(S3Error, Nothing)
+    
+    S3Response() = new(0, "", "", "", Dict{String,String,}(), nothing, nothing)
 end
 
-# NOTE: Send Content-MD5 header with all requests having a body....
+# TODO: Send Content-MD5 header with all requests having a body....
+# TODO: Support IO objects as an interface for file PUT and GET
 
-function get()
-    return (S3_Response_State, ListAllMyBucketsResult)
-end
+get() = do_request(:GET, "", "", nothing, nothing, ListAllMyBucketsResult())
 
-del_bucket(bname) = del_bucket_common(bname, "")
-del_bucket_cors(bname) = del_bucket_common(bname, "cors")
-del_bucket_lifecycle(bname) = del_bucket_common(bname, "lifecycle")
-del_bucket_policy(bname) = del_bucket_common(bname, "policy")
-del_bucket_tagging(bname) = del_bucket_common(bname, "tagging")
-del_bucket_website(bname) = del_bucket_common(bname, "website")
 
-function del_bucket_common(bucket_name, operation)
-    return (S3_Response_State)
-end
+del_bucket(bucket::String) = do_request(:DELETE, bucket, "")
+del_bucket_cors(bucket::String) = do_request(:DELETE, bucket, "cors")
+del_bucket_lifecycle(bucket::String) = do_request(:DELETE, bucket, "lifecycle")
+del_bucket_policy(bucket::String) = do_request(:DELETE, bucket, "policy")
+del_bucket_tagging(bucket::String) = do_request(:DELETE, bucket, "tagging")
+del_bucket_website(bucket::String) = do_request(:DELETE, bucket, "website")
 
 type GetBucketOptions
     delimiter::Union(String, Nothing)
@@ -37,15 +47,15 @@ type GetBucketOptions
     prefix::Union(String, Nothing)
 end
 
-get_bucket(bname, options::Union(GetBucketOptions, Nothing)) = get_common(bname, "", options, ListBucketResult())
-get_bucket_acl(bname) = get_common(bname, "acl", nothing, AccessControlPolicy())
-get_bucket_cors(bname) = get_common(bname, "cors", nothing, CORSConfiguration())
-get_bucket_lifecycle(bname) = get_common(bname, "lifecycle", nothing, LifecycleConfiguration())
-get_bucket_policy(bname) = get_common(bname, "policy", nothing, string())
-get_bucket_location(bname) = get_common(bname, "location", nothing, LocationConstraint())
-get_bucket_logging(bname) = get_common(bname, "logging", nothing, BucketLoggingStatus())
-get_bucket_notification(bname) = get_common(bname, "notification", nothing, NotificationConfiguration())
-get_bucket_tagging(bname) = get_common(bname, "tagging", nothing, Tagging())
+get_bucket(bucket::String, options::Union(GetBucketOptions, Nothing)) = do_request(:GET, bucket, "", options, nothing, resp_obj=ListBucketResult())
+get_bucket_acl(bucket::String) = do_request(:GET, bucket, "acl", nothing, nothing, resp_obj=AccessControlPolicy())
+get_bucket_cors(bucket::String) = do_request(:GET, bucket, "cors", nothing, nothing, resp_obj=CORSConfiguration())
+get_bucket_lifecycle(bucket::String) = do_request(:GET, bucket, "lifecycle", nothing, nothing, resp_obj=LifecycleConfiguration())
+get_bucket_policy(bucket::String) = do_request(:GET, bucket, "policy", nothing, nothing, resp_obj=:STRING)
+get_bucket_location(bucket::String) = do_request(:GET, bucket, "location", nothing, nothing, resp_obj=LocationConstraint())
+get_bucket_logging(bucket::String) = do_request(:GET, bucket, "logging", nothing, nothing, resp_obj=BucketLoggingStatus())
+get_bucket_notification(bucket::String) = do_request(:GET, bucket, "notification", nothing, nothing, resp_obj=NotificationConfiguration())
+get_bucket_tagging(bucket::String) = do_request(:GET, bucket, "tagging", nothing, nothing, resp_obj=Tagging())
 
 type GetBucketObjectVersionsOptions
     delimiter::Union(String, Nothing)
@@ -55,11 +65,11 @@ type GetBucketObjectVersionsOptions
     version_id_marker::Union(String, Nothing)
 end
 
-get_bucket_object_versions(bname, options::Union(GetBucketObjectVersionsOptions, Nothing)) = get_common(bname, "versions", options, ListVersionsResult())
+get_bucket_object_versions(bucket::String, options::Union(GetBucketObjectVersionsOptions, Nothing)) = do_request(:GET, bucket, "versions", options, nothing, resp_obj=ListVersionsResult())
 
-get_bucket_request_payment(bname) = get_common(bname, "requestPayment", nothing, RequestPaymentConfiguration())
-get_bucket_versioning(bname) = get_common(bname, "versioning", nothing, VersioningConfiguration())
-get_bucket_website(bname) = get_common(bname, "website", nothing, WebsiteConfiguration())
+get_bucket_request_payment(bucket::String) = do_request(:GET, bucket, "requestPayment", nothing, nothing, resp_obj=RequestPaymentConfiguration())
+get_bucket_versioning(bucket::String) = do_request(:GET, bucket, "versioning", nothing, nothing, resp_obj=VersioningConfiguration())
+get_bucket_website(bucket::String) = do_request(:GET, bucket, "website", nothing, nothing, resp_obj=WebsiteConfiguration())
 
 
 type GetBucketUploadsOptions
@@ -69,14 +79,11 @@ type GetBucketUploadsOptions
     prefix::Union(String, Nothing)
     upload_id_marker::Union(String, Nothing)
 end
-get_bucket_multipart_uploads(bname) = get_common(bname, "uploads", nothing, ListMultipartUploadsResult())
-
-function get_common(bname, req_tag, params, resp)
-    return(S3_Response_State, resp)
-end
+get_bucket_multipart_uploads(bucket::String, options::Union(GetBucketObjectVersionsOptions, Nothing)) = do_request(:GET, bucket, "uploads", options, nothing, resp_obj=ListMultipartUploadsResult())
 
 
-function test_bucket(bname)
+
+function test_bucket(bucket::String)
 end
 
 type S3_ACL_Grantee
@@ -85,45 +92,35 @@ type S3_ACL_Grantee
     uri::Union(String, Nothing)
 end
 
-type BucketACL
-    x_acl::Union(String, Nothing)
-    x_grant_read::Union(Array{S3_ACL_Grantee, 1}, Nothing)
-    x_grant_write::Union(Array{S3_ACL_Grantee, 1}, Nothing)   
-    x_grant_read_acp::Union(Array{S3_ACL_Grantee, 1}, Nothing)    
-    x_grant_write_acp::Union(Array{S3_ACL_Grantee, 1}, Nothing)   
-    x_grant_full_control::Union(Array{S3_ACL_Grantee, 1}, Nothing)
+type S3_ACL
+    acl::Union(String, Nothing)
+    grant_read::Union(Array{S3_ACL_Grantee, 1}, Nothing)
+    grant_write::Union(Array{S3_ACL_Grantee, 1}, Nothing)   
+    grant_read_acp::Union(Array{S3_ACL_Grantee, 1}, Nothing)    
+    grant_write_acp::Union(Array{S3_ACL_Grantee, 1}, Nothing)   
+    grant_full_control::Union(Array{S3_ACL_Grantee, 1}, Nothing)
 end
 
 
-function create_bucket(bname, acl::Union(BucketACL, Nothing), location_constraint::Union(String, Nothing))
-    return put_common(bname, "", params, req_body, nothing)
+function create_bucket(bucket::String, acl::Union(S3_ACL, Nothing), location_constraint::Union(String, Nothing))
+    return do_request(:PUT, bucket, "", params, req_body, nothing)
 end
 
 
-set_bucket_acl(bname, acl::Union(BucketACL, Nothing)) = put_common(bname, "acl", acl, nothing, nothing)
-function set_bucket_cors(bname, cors::CORSConfiguration) 
-    # params is Content-MD5 of XMLified cors
-    put_common(bname, "cors", params, cors_xml, nothing)
-end
+set_bucket_acl(bucket::String, acl::Union(S3_ACL, Nothing)) = do_request(:PUT, bucket, "acl", acl)
+set_bucket_cors(bucket::String, cors::CORSConfiguration) = do_request(:PUT, bucket, "cors", nothing, cors_xml)
+set_bucket_lifecycle(bucket::String, lc::LifecycleConfiguration) = do_request(:PUT, bucket, "lifecycle", nothing, lc_xml)
+set_bucket_policy(bucket::String, policy_json::String) = do_request(:PUT, bucket, "policy", nothing, policy_json)
 
-
-
-function set_bucket_lifecycle(bname, lc::LifecycleConfiguration) 
-    # params is Content-MD5 of XMLified lf
-    put_common(bname, "lifecycle", params, lc_xml, nothing)
-end
-
-set_bucket_policy(bname, policy_json::String) = put_common(bname, "policy", nothing, policy_json, nothing)
-
-function set_bucket_logging(bname, logging::BucketLoggingStatus)
-    put_common(bname, "logging", nothing, logging_xml, nothing)
+function set_bucket_logging(bucket::String, logging::BucketLoggingStatus)
+    do_request(:PUT, bucket, "logging", nothing, logging_xml)
 end
 
 
 
-function set_bucket_notification(bname, notif::NotificationConfiguration)
+function set_bucket_notification(bucket::String, notif::NotificationConfiguration)
     # NOTE: x-amz-sns-test-message-id is returned as a header - needs to be extracted....
-    put_common(bname, "notification", nothing, nothing, nothing)
+    do_request(:PUT, bucket, "notification", nothing, notif_xml)
 end
 
 type Tag
@@ -137,29 +134,182 @@ type Tagging
 end
 
 
-function set_bucket_tagging(bname, tagging::Tagging)
-    # Content-MD5
-    put_common(bname, "tagging", nothing, tagging_xml, nothing)
-end
+set_bucket_tagging(bucket::String, tagging::Tagging) = do_request(:PUT, bucket, "tagging", nothing, tagging_xml)
+set_bucket_request_payment(bucket::String, pay::RequestPaymentConfiguration) = do_request(:PUT, bucket, "requestPayment", nothing, pay_xml)
 
-
-function set_bucket_request_payment(bname, pay::RequestPaymentConfiguration)
-    put_common(bname, "requestPayment", nothing, pay_xml, nothing)
-end
-
-
-function set_bucket_versioning(bname, mfa::Union(String, Nothing), config::VersioningConfiguration)
+function set_bucket_versioning(bucket::String, mfa::Union(String, Nothing), config::VersioningConfiguration)
     #x-amz-mfa is not mandatory if status is suspended
-    put_common(bname, "versioning", params, config_xml, nothing)
+    do_request(:PUT, bucket, "versioning", params, config_xml)
 
 end
-function set_bucket_website(bname, config::WebsiteConfiguration)
-    put_common(bname, "website", nothing, config_xml, nothing)
+set_bucket_website(bucket::String, config::WebsiteConfiguration) = do_request(:PUT, bucket, "website", nothing, config_xml)
+
+
+######################################
+### OPERATIONS ON OBJECTS
+######################################
+
+function del_object(bucket::String, key::String, version=nothing, mfa=nothing) 
+    #TODO - handle version and mfa
+    do_request(:DELETE, bucket, key, hp, qp)
 end
 
-function put_common(bname, req_tag, params, req_body, resp)
-    return(S3_Response_State, resp)
+type ObjectType
+    key::String
+    versionId::Union(String, Nothing)
+end
+
+type DeleteObjectsType
+    object::Array{ObjectType, 1}
+    quiet::Union(Bool, Nothing)
 end
 
 
+function del_object_multi(bucket::String, delset::DeleteObjectsType, mfa=nothing)
+    # TODO : Make XML out of delset
+    #TODO - handle mfa
+    do_request(:POST, bucket, "delete", query_params=, req_body=, resp_obj=DeleteResult())
+end
+
+type GetObjectOptions
+    # These go into the query string
+    response_content_type::Union(String, Nothing)   
+    response_content_language::Union(String, Nothing)   
+    response_expires::Union(String, Nothing)   
+    response_cache_control::Union(String, Nothing)   
+    response_content_disposition::Union(String, Nothing)   
+    response_content_encoding::Union(String, Nothing)   
+    
+    versionId::Union(String, Nothing)
+
+    # These go into the header
+    range::Union(String, Nothing)   
+    if_modified_since::Union(String, Nothing)   
+    if_unmodified_since::Union(String, Nothing)   
+    if_match::Union(String, Nothing)   
+    if_none_match::Union(String, Nothing)   
+end
+
+function get_object(bucket::String, key::String, options::GetObjectOptions, outstream=nothing)
+    # write to out stream if it is an IO Stream, else return an Array of Uint8s
+    # TODO : Split options between hdr and query params...
+    do_request(:GET, bucket, key, hdr_params=, query_params=, resp_ostream=outstream)
+end
+
+function get_object_acl(bucket::String, key::String, version=nothing)
+    do_request(:GET, bucket, key, query_params=, resp_obj=AccessControlPolicy())
+end
+
+function get_object_torrent(bucket::String, key) 
+    # set torrent
+    do_request(:GET, bucket, key, query_params=, resp_obj=:BYTE_ARRAY)
+end
+
+function describe_object(bucket::String, key::String, options::GetObjectOptions)
+    # TODO : Split options between hdr and query params...
+    do_request(:HEAD, bucket, key, hdr_params=, query_params=)
+end
+
+function test_object(bucket::String, key::String, origin::Union(String, Nothing), method::Union(String, Nothing), headers::Union(String, Nothing))
+    do_request(:OPTIONS, bucket, key, hdr_params=)
+end
+
+#POST Object
+
+function restore_object(bucket::String, key::String, days::Int)
+    # TODO qp = "restore"
+    # convert days to RestoreRequest
+    do_request(:RESTORE, bucket, key, hdr_params=, query_params=)
+end
+
+type PutObjectOptions
+# Standard HTTP headers
+    cache_control::Union(String, Nothing)
+    content_disposition::Union(String, Nothing)
+    content_encoding::Union(String, Nothing)
+    content_type::Union(String, Nothing)
+#Expect  not supported...
+    expires::Union(DateTime, Nothing)
+    
+# x-amz header fields    
+    meta::Union(Dict{String, String}, Nothing)
+    server_side​_encryption::Union(String, Nothing)   
+    storage_class::Union(String, Nothing) 
+    website​_redirect_location::Union(String, Nothing)    
+    acl::Union(S3_ACL, Nothing)
+end
+
+function put_object(bucket::String, key::String, options::PutObjectOptions, data::Union(IOStream, String))
+    do_request(:PUT, bucket, key, query_params=, req_istream=instream, req_body=)
+end
+
+function put_object_acl(bucket::String, key::String, acl::AccessControlPolicy, version=Union(String, Nothing))
+    do_request(:PUT, bucket, key , query_params="?acl", req_body=acl_xml)
+end
+
+type CopyMatchOptions
+# below prefixed with x-amz-copy-source-
+    if_match::Union(String, Nothing)
+    if_none_match::Union(String, Nothing) 
+    if_unmodified_since::Union(String, Nothing)
+    if_modified_since::Union(String, Nothing)
+end
+
+type CopyObjectOptions
+# All are x-amz options
+    copy_source::String  
+    metadata_directive::Union(String, Nothing)
+    match_options::Union(CopyMatchOptions, Nothing)
+    
+# x-amz only header fields    
+    meta::Union(Dict{String, String}, Nothing)
+    server_side​_encryption::Union(String, Nothing)   
+    storage_class::Union(String, Nothing) 
+    website​_redirect_location::Union(String, Nothing)    
+    acl::Union(S3_ACL, Nothing)
+end
+
+function copy_object(dest_bucket::String, dest_key::String, options::CopyObjectOptions)
+    do_request(:PUT, dest_bucket, dest_key, hdr_params=, resp_obj = CopyObjectResult())
+end
+
+function initiate_multipart_upload(bucket::String, key::String, options::PutObjectOptions)
+    do_request(:POST, bucket, key, query_params="uploads", hdr_params=, resp_obj = InitiateMultipartUploadResult())
+end
+
+function upload_part(bucket::String, key::String, part_number::String, upload_id::String, data::Union(IOStream, String))
+    do_request(:PUT, bucket, key, query_params=, hdr_params=, resp_obj=, req_istream=)
+end
+
+type CopyUploadPartOptions
+    copy_source::String   
+    source_range::Union(String, Nothing)
+    match_options::Union(CopyMatchOptions, Nothing)
+end
+
+function copy_upload_part(bucket::String, key::String, part_number::String, upload_id::String, options::CopyUploadPartOptions)
+    do_request(:PUT, bucket, key, query_params=, hdr_params=, resp_obj=CopyPartResult())
+end
+
+type S3PartTag
+    part_number::String
+    etag::String
+end
+
+function complete_multipart_upload(bucket::String, key::String, upload_id::String, part_ids::Vector{S3PartTag})
+    do_request(:POST, bucket, key, query_params=upload_id..., req_body=req_xml, resp_obj=CompleteMultipartUploadResult())
+end
+
+
+function abort_multipart_upload(bucket::String, key::String, upload_id::String)
+    do_request(:DELETE, bucket, key, query_params=upload_id...)
+end
+
+function list_upload_parts(bucket::String, key::String, upload_id::String)
+    do_request(:GET, bucket, key, query_params=upload_id..., resp_obj=ListPartsResult())
+end
+
+function do_request(verb, bucket, req_tag; hdr_params=nothing, query_params=nothing, req_body=nothing, req_istream=nothing, resp_obj=nothing, resp_ostream=nothing)
+    return(S3Response, resp)
+end
 
