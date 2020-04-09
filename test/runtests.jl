ab5f24b6790d2bf4df89dbdb3488e93e990449bc
Matt Brzezinski <matt.brzezinski@invenia.ca>
2020-04-09 11:16:16 -0500

    Reorder runtests using

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using AWS
+using AWS: AWSCredentials
 using AWSCore
 using AWSCore: Services
-using AWS: AWSCredentials
 using AWS.AWSMetadataUtilities: _documentation_cleaning, _filter_latest_service_version,
     _generate_low_level_definition, _generate_high_level_definition, _generate_high_level_definitions,
     _get_aws_sdk_js_files, _get_service_and_version, _get_function_parameters,
