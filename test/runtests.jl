c94a154379d5cbb69f3d1d4ff64e1c29829d9d31
Matt Brzezinski <matt.brzezinski@invenia.ca>
2020-04-09 11:13:54 -0500

    Ported over AWSCredentials Tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,22 @@
 using AWS
+using AWSCore
+using AWSCore: Services
+using AWS: AWSCredentials
 using AWS.AWSMetadataUtilities: _documentation_cleaning, _filter_latest_service_version,
     _generate_low_level_definition, _generate_high_level_definition, _generate_high_level_definitions,
     _get_aws_sdk_js_files, _get_service_and_version, _get_function_parameters,
     InvalidFileName, ProtocolNotDefined
+using Dates
+using HTTP
 using JSON
 using OrderedCollections: OrderedDict
+using Mocking
+using Retry
 using Test
 
+Mocking.activate()
+aws = aws_config()
+
 include("AWS.jl")
+include("AWSCredentials.jl")
 include("AWSMetadataUtilities.jl")
