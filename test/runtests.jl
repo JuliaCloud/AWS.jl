2cf073d6a4bea793bdaf9357373bce4b238c2345
Matt Brzezinski <matt.brzezinski@invenia.ca>
2020-08-10 18:13:21 -0500

    _documentation_cleaning -> _clean_documentation

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using AWS
 using AWS: AWSCredentials
 using AWS: AWSServices
 using AWS.AWSExceptions: AWSException
-using AWS.AWSMetadataUtilities: _documentation_cleaning, _filter_latest_service_version,
+using AWS.AWSMetadataUtilities: _clean_documentation, _filter_latest_service_version,
     _generate_low_level_definition, _generate_high_level_definition, _generate_high_level_definitions,
     _get_aws_sdk_js_files, _get_service_and_version, _get_function_parameters, _clean_uri,
     InvalidFileName, ProtocolNotDefined
