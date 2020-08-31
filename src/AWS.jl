d836987db314f4db2910a8f25b10948000f69e45
Matt Brzezinski <matt.brzezinski@invenia.ca>
2020-08-31 13:28:48 -0500

    Minor fixes
    - Properly reference the global AWSConfig, in each protocol function
    - Remove response_dict_type from args if it's available
    - Use response_dict_type in Query and JSON services

--- src/AWS.jl
+++ src/AWS.jl
@@ -485,7 +485,7 @@ Perform a RestXML request to AWS.
 """
 function (service::RestXMLService)(
     request_method::String, request_uri::String, args::AbstractDict{String, <:Any}=Dict{String, Any}(); 
-    aws_config::AWSConfig=aws_config[],
+    aws_config::AWSConfig=global_aws_config(),
 )
     request = Request(
         service=service.name,
@@ -501,6 +501,7 @@ function (service::RestXMLService)(
 
     if haskey(args, "response_dict_type")
         request.response_dict_type = args["response_dict_type"]
+        delete!(args, "response_dict_type")
     end
 
     delete!(args, "headers")
@@ -544,7 +545,7 @@ Perform a Query request to AWS.
 """
 function (service::QueryService)(
     operation::String, args::AbstractDict{String, <:Any}=Dict{String, Any}(); 
-    aws_config::AWSConfig=aws_config[],
+    aws_config::AWSConfig=global_aws_config(),
 )
     POST_RESOURCE = "/"
     return_headers = _return_headers(args)
@@ -561,6 +562,11 @@ function (service::QueryService)(
         response_stream=get(args, "response_stream", nothing),
         return_raw=get(args, "return_raw", false),
     )
+
+    if haskey(args, "response_dict_type")
+        request.response_dict_type = args["response_dict_type"]
+        delete!(args, "response_dict_type")
+    end
 
     request.headers["Content-Type"] = "application/x-www-form-urlencoded; charset=utf-8"
 
@@ -591,7 +597,7 @@ Perform a JSON request to AWS.
 """
 function (service::JSONService)(
     operation::String, args::AbstractDict{String, <:Any}=Dict{String, Any}();
-    aws_config::AWSConfig=aws_config[],
+    aws_config::AWSConfig=global_aws_config(),
 )
     POST_RESOURCE = "/"
     return_headers = _return_headers(args)
@@ -609,6 +615,11 @@ function (service::JSONService)(
         response_stream=get(args, "response_stream", nothing),
         return_raw=get(args, "return_raw", false),
     )
+
+    if haskey(args, "response_dict_type")
+        request.response_dict_type = args["response_dict_type"]
+        delete!(args, "response_dict_type")
+    end
 
     request.headers["Content-Type"] = "application/x-amz-json-$(service.json_version)"
     request.headers["X-Amz-Target"] = "$(service.target).$(operation)"
@@ -637,7 +648,7 @@ Perform a RestJSON request to AWS.
 """
 function (service::RestJSONService)(
     request_method::String, request_uri::String, args::AbstractDict{String, <:Any}=Dict{String, String}();
-    aws_config::AWSConfig=aws_config[],
+    aws_config::AWSConfig=global_aws_config(),
 )
     return_headers = _return_headers(args)
 
@@ -655,6 +666,7 @@ function (service::RestJSONService)(
 
     if haskey(args, "response_dict_type")
         request.response_dict_type = args["response_dict_type"]
+        delete!(args, "response_dict_type")
     end
 
     request.url = _generate_service_url(aws_config.region, request.service, request.resource)
