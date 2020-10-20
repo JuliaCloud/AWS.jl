3562f633a5ddc2f8f42669557e00a352296292cf
Matt Brzezinski <matt.brzezinski@invenia.ca>
2020-10-20 10:48:51 -0500

    Try put_object()

--- test/issues.jl
+++ test/issues.jl
@@ -7,9 +7,9 @@ S3.create_bucket(bucket_name)
     # https://github.com/JuliaCloud/AWS.jl/issues/223
     body = "Hello World!"
     file_name = "contains spaces"
-    S3.put_object(bucket_name, file_name, Dict("body" => body))
 
     try
+        S3.put_object(bucket_name, file_name, Dict("body" => body))
         resp = S3.get_object(bucket_name, file_name)
 
         @test String(resp) == body
