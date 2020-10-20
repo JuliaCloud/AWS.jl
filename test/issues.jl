ac48b57b9c854a0d104dca324b3847518c0f2fe6
Matt Brzezinski <matt.brzezinski@invenia.ca>
2020-10-20 16:48:06 -0500

    Added additional tests

--- test/issues.jl
+++ test/issues.jl
@@ -20,9 +20,31 @@ S3.create_bucket(bucket_name)
 end
 
 @testset "issue 227" begin
-    # https://github.com/JuliaCloud/AWS.jl/issues/227
-    config = AWSConfig(creds=nothing)
-    resp = S3.get_object("www.invenia.ca", "index.html"; aws_config=config)
+    @testset "s3 public bucket" begin
+        # https://github.com/JuliaCloud/AWS.jl/issues/227
+        config = AWSConfig(creds=nothing)
+        resp = S3.get_object("www.invenia.ca", "index.html"; aws_config=config)
 
-    @test !isempty(resp)
+        @test !isempty(resp)
+    end
+
+    @testset "s3 private bucket" begin
+        bucket_name = "aws-jl-test-issues---" * _now_formatted()
+        file_name = "hello_world"
+
+        try
+            S3.create_bucket(bucket_name)
+            S3.put_object(bucket_name, file_name)
+
+            @test_throws AWSException S3.get_object(bucket_name, file_name; aws_config=AWSConfig(creds=nothing))
+        finally
+            S3.delete_bucket(bucket_name)
+        end
+    end
+
+    @testset "lambda" begin
+        @service Lambda
+
+        @test_throws AWSException Lambda.list_functions(;aws_config=AWSConfig(creds=nothing))
+    end
 end
