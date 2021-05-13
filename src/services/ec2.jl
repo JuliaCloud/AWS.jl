62e6ff7df3290984912b5e4bb917f68fe4e33ac4
mattBrzezinski <mattBrzezinski@users.noreply.github.com>
2021-05-13 06:11:58 +0000

    AWS API Definitions Updated

--- src/services/ec2.jl
+++ src/services/ec2.jl
@@ -6694,20 +6694,20 @@ Optional parameters can be passed as a `params::Dict{String,<:Any}`. Valid keys 
   description. Instances that include (Amazon VPC) in the product platform description will
   only be displayed to EC2-Classic account holders and are for use with Amazon VPC
   (Linux/UNIX | Linux/UNIX (Amazon VPC) | SUSE Linux | SUSE Linux (Amazon VPC) | Red Hat
-  Enterprise Linux | Red Hat Enterprise Linux (Amazon VPC) | Red Hat Enterprise Linux with HA
-  (Amazon VPC) | Windows | Windows (Amazon VPC) | Windows with SQL Server Standard | Windows
-  with SQL Server Standard (Amazon VPC) | Windows with SQL Server Web | Windows with SQL
-  Server Web (Amazon VPC) | Windows with SQL Server Enterprise | Windows with SQL Server
-  Enterprise (Amazon VPC)).    reserved-instances-id - The ID of the Reserved Instance.
-  start - The time at which the Reserved Instance purchase request was placed (for example,
-  2014-08-07T11:54:42.000Z).    state - The state of the Reserved Instance (payment-pending |
-  active | payment-failed | retired).    tag:&lt;key&gt; - The key/value combination of a tag
-  assigned to the resource. Use the tag key in the filter name and the tag value as the
-  filter value. For example, to find all resources that have a tag with the key Owner and the
-  value TeamA, specify tag:Owner for the filter name and TeamA for the filter value.
-  tag-key - The key of a tag assigned to the resource. Use this filter to find all resources
-  assigned a tag with a specific key, regardless of the tag value.    usage-price - The usage
-  price of the Reserved Instance, per hour (for example, 0.84).
+  Enterprise Linux | Red Hat Enterprise Linux (Amazon VPC) | Windows | Windows (Amazon VPC) |
+  Windows with SQL Server Standard | Windows with SQL Server Standard (Amazon VPC) | Windows
+  with SQL Server Web | Windows with SQL Server Web (Amazon VPC) | Windows with SQL Server
+  Enterprise | Windows with SQL Server Enterprise (Amazon VPC)).    reserved-instances-id -
+  The ID of the Reserved Instance.    start - The time at which the Reserved Instance
+  purchase request was placed (for example, 2014-08-07T11:54:42.000Z).    state - The state
+  of the Reserved Instance (payment-pending | active | payment-failed | retired).
+  tag:&lt;key&gt; - The key/value combination of a tag assigned to the resource. Use the tag
+  key in the filter name and the tag value as the filter value. For example, to find all
+  resources that have a tag with the key Owner and the value TeamA, specify tag:Owner for the
+  filter name and TeamA for the filter value.    tag-key - The key of a tag assigned to the
+  resource. Use this filter to find all resources assigned a tag with a specific key,
+  regardless of the tag value.    usage-price - The usage price of the Reserved Instance, per
+  hour (for example, 0.84).
 - `"OfferingClass"`: Describes whether the Reserved Instance is Standard or Convertible.
 - `"ReservedInstancesId"`: One or more Reserved Instance IDs. Default: Describes all your
   Reserved Instances, or only those otherwise specified.
@@ -6811,13 +6811,13 @@ Optional parameters can be passed as a `params::Dict{String,<:Any}`. Valid keys 
   include (Amazon VPC) in the product platform description will only be displayed to
   EC2-Classic account holders and are for use with Amazon VPC. (Linux/UNIX | Linux/UNIX
   (Amazon VPC) | SUSE Linux | SUSE Linux (Amazon VPC) | Red Hat Enterprise Linux | Red Hat
-  Enterprise Linux (Amazon VPC) | Red Hat Enterprise Linux with HA (Amazon VPC) | Windows |
-  Windows (Amazon VPC) | Windows with SQL Server Standard | Windows with SQL Server Standard
-  (Amazon VPC) | Windows with SQL Server Web |  Windows with SQL Server Web (Amazon VPC) |
-  Windows with SQL Server Enterprise | Windows with SQL Server Enterprise (Amazon VPC))
-  reserved-instances-offering-id - The Reserved Instances offering ID.    scope - The scope
-  of the Reserved Instance (Availability Zone or Region).    usage-price - The usage price of
-  the Reserved Instance, per hour (for example, 0.84).
+  Enterprise Linux (Amazon VPC) | Windows | Windows (Amazon VPC) | Windows with SQL Server
+  Standard | Windows with SQL Server Standard (Amazon VPC) | Windows with SQL Server Web |
+  Windows with SQL Server Web (Amazon VPC) | Windows with SQL Server Enterprise | Windows
+  with SQL Server Enterprise (Amazon VPC))     reserved-instances-offering-id - The Reserved
+  Instances offering ID.    scope - The scope of the Reserved Instance (Availability Zone or
+  Region).    usage-price - The usage price of the Reserved Instance, per hour (for example,
+  0.84).
 - `"IncludeMarketplace"`: Include Reserved Instance Marketplace offerings in the response.
 - `"InstanceType"`: The instance type that the reservation will cover (for example,
   m1.small). For more information, see Instance types in the Amazon EC2 User Guide.
