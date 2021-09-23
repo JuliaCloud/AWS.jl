a4fea25316193afdcde3304f4ca827e02060ff74
Curtis Vogt <curtis.vogt@gmail.com>
2021-09-23 09:52:52 -0500

    MinIO tests cleanup bucket on failure

--- test/minio.jl
+++ test/minio.jl
@@ -28,43 +28,46 @@ datadir = joinpath(dirname(pathof(AWS)), "..", "data")
 
 # Create bucket and place objects in it
 S3.create_bucket("anewbucket")
-S3.put_object("anewbucket", "myobject", Dict("body" => "Hi from Minio"))
-S3.put_object("anewbucket", "empty")
-S3.put_object("anewbucket", "foo/bar", Dict("body" => "a nested object"))
-S3.put_object("anewbucket", "foo/baz", Dict("body" => "a secondnested object"))
+try
+    S3.put_object("anewbucket", "myobject", Dict("body" => "Hi from Minio"))
+    S3.put_object("anewbucket", "empty")
+    S3.put_object("anewbucket", "foo/bar", Dict("body" => "a nested object"))
+    S3.put_object("anewbucket", "foo/baz", Dict("body" => "a secondnested object"))
 
-# Test retrieving an object
-@test String(S3.get_object("anewbucket", "myobject")) == "Hi from Minio"
+    # Test retrieving an object
+    @test String(S3.get_object("anewbucket", "myobject")) == "Hi from Minio"
 
-# Test retrieving an object into a stream target
-mktemp() do f, io
-    S3.get_object("anewbucket", "myobject", Dict("response_stream" => io))
-    @test read(f, String) == "Hi from Minio"
+    # Test retrieving an object into a stream target
+    mktemp() do f, io
+        S3.get_object("anewbucket", "myobject", Dict("response_stream" => io))
+        @test read(f, String) == "Hi from Minio"
+    end
+
+    # Test listing
+    objs = S3.list_objects_v2("anewbucket")
+    @test length(objs["Contents"]) == 4
+
+    # Test api version 2 of list-objects
+    objs_truncated = S3.list_objects_v2("anewbucket", Dict("max-keys" => 2))
+    @test length(objs_truncated["Contents"]) == 2
+    @test objs_truncated["IsTruncated"] == "true"
+    @test haskey(objs_truncated, "NextContinuationToken")
+
+    # Test listing with prefixes
+    objs_prefix = S3.list_objects_v2("anewbucket", Dict("prefix" => "", "delimiter" => "/"))
+    @test length(objs_prefix["Contents"]) == 2
+    @test sort(getindex.(objs_prefix["Contents"], "Key")) == ["empty", "myobject"]
+    @test objs_prefix["CommonPrefixes"]["Prefix"] == "foo/"
+
+finally
+    # Delete all objects and the bucket
+    objs = S3.list_objects_v2("anewbucket")
+
+    for obj in objs["Contents"]
+        S3.delete_object("anewbucket", obj["Key"])
+    end
+
+    S3.delete_bucket("anewbucket")
 end
-
-# Test listing
-objs = S3.list_objects_v2("anewbucket")
-@test length(objs["Contents"]) == 4
-
-# Test api version 2 of list-objects
-objs_truncated = S3.list_objects_v2("anewbucket", Dict("max-keys" => 2))
-@test length(objs_truncated["Contents"]) == 2
-@test objs_truncated["IsTruncated"] == "true"
-@test haskey(objs_truncated, "NextContinuationToken")
-
-# Test listing with prefixes
-objs_prefix = S3.list_objects_v2("anewbucket", Dict("prefix" => "", "delimiter" => "/"))
-@test length(objs_prefix["Contents"]) == 2
-@test sort(getindex.(objs_prefix["Contents"], "Key")) == ["empty", "myobject"]
-@test objs_prefix["CommonPrefixes"]["Prefix"] == "foo/"
-
-# And delete all objects and the bucket
-objs = S3.list_objects_v2("anewbucket")
-
-for obj in objs["Contents"]
-    S3.delete_object("anewbucket", obj["Key"])
-end
-
-S3.delete_bucket("anewbucket")
 
 @test isempty(filter(!isequal(".minio.sys"), readdir(datadir)))
