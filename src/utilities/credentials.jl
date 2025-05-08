41b293d3666bf06bb541ffd690256f404a649242
Octogonapus <me@irrational.engineering>
2025-05-08 00:19:13 -0400

    Support IAM Identity Center

--- src/utilities/credentials.jl
+++ src/utilities/credentials.jl
@@ -25,11 +25,20 @@ end
 """
 Get the value for `key` in the `ini` file for a given `profile`.
 """
+_get_profile_value(ini::Inifile, profile::AbstractString, key::AbstractString; kwargs...) =
+    _get_ini_value(ini, "profile", profile, key; kwargs...)
+
+"""
+Get the value for `key` in the `ini` file for a given `sso_session`.
+"""
+_get_sso_session_value(ini::Inifile, sso_session::AbstractString, key::AbstractString; kwargs...) =
+    _get_ini_value(ini, "sso-session", sso_session, key; kwargs...)
+
 function _get_ini_value(
-    ini::Inifile, profile::AbstractString, key::AbstractString; default_value=nothing
+    ini::Inifile, type::AbstractString, type_value::AbstractString, key::AbstractString; default_value=nothing
 )
-    value = get(ini, "profile $profile", key)
-    value === :notfound && (value = get(ini, profile, key))
+    value = get(ini, "$type $type_value", key)
+    value === :notfound && (value = get(ini, type_value, key))
     value === :notfound && (value = default_value)
 
     return value
@@ -121,9 +130,9 @@ Get `AWSCredentials` for the specified `profile` from the `inifile`. If targetin
 the default credentials will be returned.
 """
 function _aws_get_credential_details(profile::AbstractString, ini::Inifile)
-    access_key = _get_ini_value(ini, profile, "aws_access_key_id")
-    secret_key = _get_ini_value(ini, profile, "aws_secret_access_key")
-    token = _get_ini_value(ini, profile, "aws_session_token"; default_value="")
+    access_key = _get_profile_value(ini, profile, "aws_access_key_id")
+    secret_key = _get_profile_value(ini, profile, "aws_secret_access_key")
+    token = _get_profile_value(ini, profile, "aws_session_token"; default_value="")
 
     return (access_key, secret_key, token)
 end
@@ -166,12 +175,13 @@ end
 
 """
 Retrieve the SSO access token from cache.
+Pass the session name when using sso-session.
 """
-function _sso_cache_access_token(sso_start_url::Union{AbstractString,Nothing})
-    isnothing(sso_start_url) && return nothing
+function _sso_cache_access_token(sso_start_url_or_session_name::Union{AbstractString,Nothing})
+    isnothing(sso_start_url_or_session_name) && return nothing
 
     cache_path = joinpath(homedir(), ".aws", "sso", "cache")
-    cache_file = joinpath(cache_path, bytes2hex(sha1(sso_start_url)) * ".json")
+    cache_file = joinpath(cache_path, bytes2hex(sha1(sso_start_url_or_session_name)) * ".json")
 
     !isfile(cache_file) && return nothing
 
@@ -185,13 +195,32 @@ end
 Retrieve sso-specific details for the given `profile`.
 """
 function _aws_get_sso_credential_details(profile::AbstractString, ini::Inifile)
-    sso_start_url = _get_ini_value(ini, profile, "sso_start_url")
-    sso_account_id = _get_ini_value(ini, profile, "sso_account_id")
-    sso_role_name = _get_ini_value(ini, profile, "sso_role_name"; default_value="default")
-    sso_region = _get_ini_value(ini, profile, "sso_region"; default_value=DEFAULT_REGION)
+    sso_session = _get_profile_value(ini, profile, "sso_session")
+    sso_account_id = _get_profile_value(ini, profile, "sso_account_id") # optional but we require it
+    sso_role_name = _get_profile_value(ini, profile, "sso_role_name"; default_value="default") # optional but we require it
+    access_token = if sso_session !== nothing
+        # new configuration
+        # https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-sso.html
+        sso_region = _get_sso_session_value(ini, sso_session, "sso_region") # required
+        sso_role_name = _get_profile_value(ini, profile, "sso_role_name") # optional but we require it
+        if sso_role_name === nothing
+            error("AWS.jl requires sso_role_name is set in the profile section.")
+        end
+        # I have no idea what sso_registration_scopes is supposed to do. It's an optional parameter. I can't find any
+        # documentation for it. Everything I've tested seems to work without it. Maybe the AWS CLI uses is to cache the
+        # access token, and then we don't need to use it? No idea.
 
-    # sso cache access token
-    access_token = @mock _sso_cache_access_token(sso_start_url)
+        # the new config uses the session name instead of the start url to index into the cache
+        @mock _sso_cache_access_token(sso_session)
+    else
+        # legacy configuration
+        # https://docs.aws.amazon.com/cli/latest/userguide/sso-configure-profile-legacy.html#sso-configure-profile-manual
+        sso_start_url = _get_profile_value(ini, profile, "sso_start_url")
+        sso_region = _get_profile_value(ini, profile, "sso_region"; default_value=DEFAULT_REGION)
+    
+        # sso cache access token
+        @mock _sso_cache_access_token(sso_start_url)
+    end
 
     headers = Dict{String,Any}("x-amz-sso_bearer_token" => access_token)
     tmp_config = AWSConfig(; creds=nothing, region=sso_region)
