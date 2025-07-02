b2a626806f51284324897bab0d8e7e10d4b36f90
Curtis Vogt <curtis.vogt@gmail.com>
2025-07-02 12:23:43 -0500

    Make resursive profile merging optional

--- test/unit/AWSCredentials.jl
+++ test/unit/AWSCredentials.jl
@@ -1,7 +1,37 @@
 @testset "_aws_profile_config" begin
     using AWS: _aws_profile_config
 
-    @testset "source profile" begin
+    @testset "non-recursive" begin
+        #! format: off
+        buffer = IOBuffer(
+            """
+            [profile test]
+            output = json
+            region = us-east-1
+
+            [profile test:dev]
+            source_profile = test
+            role_arn = arn:aws:iam::123456789000:role/Dev
+            """
+        )
+        #! format: on
+        ini = Inifile()
+        read(ini, buffer)
+
+        # Only the fields from profile "test"
+        config = _aws_profile_config(ini, "test")
+        @test keys(config) ⊆ Set(["output", "region"])
+        @test config["output"] == "json"
+        @test config["region"] == "us-east-1"
+
+        # Only the fields from profile "test:dev"
+        config = _aws_profile_config(ini, "test:dev")
+        @test keys(config) ⊆ Set(["source_profile", "role_arn"])
+        @test config["source_profile"] == "test"
+        @test config["role_arn"] == "arn:aws:iam::123456789000:role/Dev"
+    end
+
+    @testset "recursive" begin
         #! format: off
         buffer = IOBuffer(
             """
@@ -23,13 +53,13 @@
         read(ini, buffer)
 
         # Only the fields from profile "test"
-        config = _aws_profile_config(ini, "test")
+        config = _aws_profile_config(ini, "test"; recursive=true)
         @test keys(config) ⊆ Set(["output", "region"])
         @test config["output"] == "json"
         @test config["region"] == "us-east-1"
 
         # Combine the profile "test:dev" section with fields from profile "test"
-        config = _aws_profile_config(ini, "test:dev")
+        config = _aws_profile_config(ini, "test:dev"; recursive=true)
         @test keys(config) ⊆ Set(["output", "region", "role_arn"])
         @test config["output"] == "json"
         @test config["region"] == "us-east-1"
@@ -41,7 +71,7 @@
         @test !haskey(section, "output")
 
         # Conflicting keys should use the first defined entry
-        config = _aws_profile_config(ini, "test:sub-dev")
+        config = _aws_profile_config(ini, "test:sub-dev"; recursive=true)
         @test keys(config) ⊆ Set(["output", "region", "role_arn"])
         @test config["output"] == "json"
         @test config["region"] == "us-east-1"
